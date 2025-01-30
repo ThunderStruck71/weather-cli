da09fdff0e619ab96350d1ece58ce973dc988f2d
Ilya <iovodovi@yandex.ru>
2025-01-30 08:50:29 +0300

    Добавлено получение значений токена и города из переменных окружения

--- services/api.service.js
+++ services/api.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service.js"
 
 export const getWeather = async (city) => {
-	const token = await getKeyValue(TOKEN_DICTIONARY.token);
+	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 
 	if (!token) {
 		throw new Error('Не задан ключ API, установите его, используя команду -t [API_TOKEN]');
