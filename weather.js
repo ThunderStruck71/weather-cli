470d9d4151cc691b97481894cfc4229924dcd8e6
Ilya <iovodovi@yandex.ru>
2025-01-30 08:57:14 +0300

    Добавлена обработка ошибок для метода получения прогноза погоды

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
 import { getArgs } from './helpers/args.js';
 import { getWeather } from './services/api.service.js';
 import { printError, printHelp, printSuccess } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getKeyValue, saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -15,6 +15,22 @@ const saveToken = async (token) => {
 		printSuccess('Токен сохранен');
 	} catch (err) {
 		printError(err.message);
+	}
+}
+
+const getForecast = async () => {
+	try {
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		console.log(weather);
+	} catch (err) {
+		if (err?.response?.status === 404) {
+			printError('Неверно указан город');
+		} else if (err?.response?.status === 401) {
+			printError('Неверно указан токен');
+		} else {
+			printError(err.message);
+		}
 	}
 }
 
@@ -31,7 +47,7 @@ const initCLI = () => {
 		return saveToken(args.t);
 	}
 	
-	getWeather('moscow');
+	return getForecast();
 }
 
 initCLI();
